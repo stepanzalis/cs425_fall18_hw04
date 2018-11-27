438dab3e1b5d3c18daf4a0e621b1de0a8b7269f9
Stepan Zalis <stepan.zalis@uhk.cz>
2018-11-27 22:17:58 +0200

    login done

---
+++ api/login.php
@@ -0,0 +1,44 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+include_once '../config/Database.php';
+include_once '../models/User.php';
+
+$database = new Database();
+$db = $database->connect();
+
+$user = new User($db);
+
+// get posted data
+$data = json_decode(file_get_contents("php://input"));
+
+$user->email = $data->email;
+$sent_password = $data->password;
+
+$email_exists = $user->emailExists();
+
+if (!$email_exists) {
+    header("HTTP/1.1 404");
+    echo json_encode(
+        array('response' => 'Not existing')
+    );
+    return;
+}
+
+if (password_verify($sent_password, $user->password)) {
+    header("HTTP/1.1 200 OK");
+    echo json_encode(
+        array('response' => 'Correct')
+    );
+} else {
+    header("HTTP/1.1 401");
+    echo json_encode(
+        array('response' => 'Forbidden')
+    );
+}
+
+
--- js/app.js
+++ js/app.js
@@ -3,8 +3,41 @@ let map;
 $(document).ready(function () {
     initMap();
     toggleModal();
-
 });
+
+function login() {
+
+    let email = $("#email").val();
+    let password = $("#password").val();
+
+    check_inputs(email, password);
+
+    let json = JSON.stringify({email: email, password: password});
+
+    $.ajax({
+        type: 'POST',
+        url: "./api/login.php",
+        data: json,
+        dataType: 'JSON',
+        success: function (response, status, xhr) {
+            window.location.href = '/cs425_hw4/index.html';
+        },
+        error: function (xhr, status, error) {
+            if (xhr.status === 404) {
+                swal("Sorry", "You have to register first!", "error");
+            } else if (xhr.status === 401) {
+                swal("Ohh", "Incorrect email or password.", "warning");
+            }
+        }
+    });
+
+}
+
+function check_inputs(email, password) {
+    if (email.empty() || password.isEmpty) {
+        return false;
+    } else return true;
+}
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -13,6 +46,7 @@ function initMap() {
     });
 }
 
+// upload image
 function readURL(input) {
 
     const width = 250, height = 200;
@@ -37,14 +71,15 @@ document.addEventListener('DOMContentLoaded', function () {
     modal_instance = M.Modal.init(modal);
 });
 
+function toggleModal() {
+    modal_instance.isOpen ? modal_instance.close() : modal_instance.open();
+}
+
 // Collapsible
 let collapsible = document.querySelector('.collapsible.expandable');
 let collapsible_instance = M.Collapsible.init(collapsible, {
     accordion: false
 });
 
-function toggleModal() {
-    modal_instance.isOpen ? modal_instance.close() : modal_instance.open();
-}
 
 
